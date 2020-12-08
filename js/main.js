85fb84b6502c0da833a63360cb23ba192aab3e87
Vadim Sabadosh <vadimsabadosh@gmail.com>
2020-12-08 15:31:21 +0200

    Задание урока 14 Часть 1  - Выполнена

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
 'use strict';
+const DomElement = function (selector, height, width, bg, fontSize, text) {  
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.text = text;
+};
 
 
-let colorTitle = document.querySelector('.color-title');
-let btnChangeColor = document.querySelector('.change-color');
+DomElement.prototype.createElement = function () {  
+    let check = this.selector.charAt(0);
+    if(check === '.'){
+        let div = document.createElement('div');
+        div.classList.add(this.selector.split('.')[1]);
+        div.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize}`;
+        div.textContent = this.text;
+        document.body.after(div);
+    }
+    if(check === '#'){
+        let p = document.createElement('p');
+        p.setAttribute('id', this.selector.split('#')[1]);
+        p.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize}`;
+        p.textContent = this.text;
+        document.body.after(p);
+    }
+};
 
-function changeColor() {  
-    let body = document.body;
-    let r, g, b, color;
 
-    r = Math.floor(Math.random() * (256));
-    g = Math.floor(Math.random() * (256));
-    b = Math.floor(Math.random() * (256));
-    color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
-    body.style.backgroundColor = color;
-    colorTitle.textContent = color;
-    btnChangeColor.style.color = color;
-}
 
-btnChangeColor.addEventListener('click', changeColor);
+
+const domElement = new DomElement('.selector', '200px', '100px', 'red', '16px', 'Любой текст');
+
+domElement.createElement();
