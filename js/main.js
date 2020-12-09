56b265f4a1d1e1ac28ec477fe5a199728712d81a
Vadim Sabadosh <vadimsabadosh@gmail.com>
2020-12-10 00:12:09 +0200

    Вторая часть задания 15 - выполнена

--- js/main.js
+++ js/main.js
@@ -1,35 +1,16 @@
 'use strict';
-const DomElement = function (selector, height, width, bg, fontSize, text) {  
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-    this.text = text;
-};
-
-
-DomElement.prototype.createElement = function () {  
-    let check = this.selector.charAt(0);
-    if(check === '.'){
-        let div = document.createElement('div');
-        div.classList.add(this.selector.split('.')[1]);
-        div.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize}`;
-        div.textContent = this.text;
-        document.body.after(div);
+class First {
+    hello(){
+        console.log('Привет я метод родителя!');
     }
-    if(check === '#'){
-        let p = document.createElement('p');
-        p.setAttribute('id', this.selector.split('#')[1]);
-        p.style.cssText = `height: ${this.height}; width: ${this.width}; background: ${this.bg}; font-size: ${this.fontSize}`;
-        p.textContent = this.text;
-        document.body.after(p);
+}
+class Second extends First {
+    hello(){
+        super.hello();
+        console.log('А я наследуемый метод!');
     }
-};
+}
+let show = new Second();
 
 
-
-
-const domElement = new DomElement('.selector', '200px', '100px', 'red', '16px', 'Любой текст');
-
-domElement.createElement();
+show.hello();
